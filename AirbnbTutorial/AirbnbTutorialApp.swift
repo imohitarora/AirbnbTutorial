ade02473324367aec0beb7f22265c1a651ac30b3
Mohit Arora <me@aboutmohit.com>
2024-03-22 12:33:39 -0400

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Mohit Arora on 2024-03-22.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
